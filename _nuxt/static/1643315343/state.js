4506be70b65596701183a55d1c2eacfe2ceb0dc7
GitHub Action <action@github.com>
2022-01-27 20:29:03 +0000

    deploy

---
+++ _nuxt/static/1643315343/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhubstaff-blog\u002F_nuxt\u002Fstatic\u002F1643315343",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fhubstaff-blog\u002F",assetsPath:"\u002Fhubstaff-blog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"54ed0fdb"}}}}(null));
